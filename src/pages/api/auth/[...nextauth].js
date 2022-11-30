dca4e8b125f233f72e1411f9b6c6c67d4b69304f
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-11-30 15:09:40 -0300

    feat: add nextAuth configuration, SessionProvider in _app.js and update SignInButton with signIn, signOut and useSession features from next-auth/react

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+      scope: 'read:user',
+    }),
+  ],
+  // secret: process.env.GITHUB_SECRET,
+};
+
+export default NextAuth(authOptions);
