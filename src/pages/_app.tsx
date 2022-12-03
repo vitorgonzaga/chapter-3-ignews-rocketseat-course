d035301414b04bd0e2795b94dfeb966555348141
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-12-03 10:42:15 -0300

    feat: add PrismicPreview and PrismicProvider in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
-import { SessionProvider } from "next-auth/react"
-import type { AppProps } from 'next/app'
-import { Header } from '../components/Header'
+import { PrismicPreview } from "@prismicio/next";
+import { PrismicProvider } from "@prismicio/react";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import Link from "next/link";
+import { repositoryName } from "../../prismicio";
+import { Header } from "../components/Header";
 
-import '../styles/global.scss'
+import "../styles/global.scss";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Header />
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <SessionProvider session={session}>
+          <Header />
+          <Component {...pageProps} />
+        </SessionProvider>
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
